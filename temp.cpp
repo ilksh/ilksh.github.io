957903820109d579ba4065fcf40957560a558bfc
Sang Hyun Kim <ksh727301@gmail.com>
2024-09-17 12:27:04 -0400

    Hash Map

--- temp.cpp
+++ temp.cpp
@@ -1,69 +1,110 @@
-template <typename T>
+#include<bits/stdc++.h>
 
-class priority_queue{
-private:
-    int cnt;    // number of elements in priority_queue
-    T heap[MAX_N];
-    
+using namespace std;
+
+// djb2 hash
+size_t djb2(const char* str) {
+   // str = "blue"
+   size_t hash = 5381;
+
+   for(; *str; ++str) {
+      hash = ((hash << 5) + hash) + *str;
+   }
+
+   return hash;
+}
+
+const int MAX_N = 1e4 + 4;
+const int MAX_LEN = 5e2 + 5;
+
+typedef struct Node{
+   char str[MAX_LEN]; // key
+   Node* next;
+} Node;
+
+// Memory Pool Technique
+int node_count;
+Node nodes[MAX_N];
+
+Node* new_node(const char str[MAX_LEN]) {
+   strcpy(nodes[node_count].str, str);
+   nodes[node_count].next = NULL;
+
+   return &nodes[node_count++];
+}
+
+class HashMap {
+   static constexpr size_t TABLE_SIZE = 1 << 12;
+   static constexpr size_t DIV = TABLE_SIZE - 1;
+   // hash % DIV
+   // hash & (TABLE_SIZE -1)
+
+   Node hash_table[TABLE_SIZE];
+
 public:
-    int size(){ return cnt; }
-    
-    priority_queue(){ cnt = 0;} // init
-    
-    void swap(int aIdx, int bIdx){
-        int temp = heap[aIdx];
-        heap[aIdx] = heap[bIdx];
-        heap[bIdx] = temp;
-    }
-    
-    void push(T item){
-        heap[++cnt] = item;
-        int cur = cnt;
-    
-        while(cur > 1){
-            int par = cur >> 1;
-        
-            if(heap[cur] < heap[par]) break;
-            
-            swap(cur, par);
-            cur = par;
-        }
-    }
-    
-    void pop(){ // move the lowest data and delete
-        heap[1] = heap[cnt--];
-        
-        int cur = 1;
-        
-        while(cur <= cnt){
-            int left =(cur << 1) <= cnt ? (cur << 1) : -1;
-            int right = (cur << 1 | 1) <= cnt ? (cur << 1 | 1): -1;
-            int child = cur; // the index of the largest value is child
-            
-            // cur node is leaf node
-            if(left == -1) break;
-            
-            if(heap[cur] < heap[left]) child = left;
-            
-            if(right != -1 && heap[child] < heap[right]) child = right;
-            
-            if(child == left){
-                swap(cur, left);
-                cur = left;
-            }
-            
-            else if(child == right){
-                swap(cur, right);
-                cur = right;
-            }
-            
-            // cur == child
-            else break;
-        }
-    }
-    
-    T top(){ return heap[1];}
-    
-    bool empty(){return cnt == 0;}
+   HashMap() {}
+
+   void init() {
+      memset(hash_table, 0, sizeof(hash_table));
+      node_count = 0;
+   }
+
+   void insert(const char str[MAX_LEN]) {
+      Node* prev_node = get_prev_node(str);
+
+      if(prev_node->next == NULL) {
+         prev_node->next = new_node(str);
+      }
+   }
+
+   void remove(const char str[MAX_LEN]) {
+      Node* prev_node = get_prev_node(str);
+
+      if(prev_node->next != NULL) {
+         prev_node->next = prev_node->next->next;
+      }
+   }
+
+   Node* get(const char str[MAX_LEN]) {
+      return get_prev_node(str)->next;
+   }
+
+private:
+   Node* get_prev_node(const char str[MAX_LEN]) {
+      // Node* prev_ptr = &hash_table[djb2(str) % TABLE_SIZE];
+      Node* prev_ptr = &hash_table[djb2(str) & DIV];
+
+      while(prev_ptr->next != NULL && strcmp(prev_ptr->next->str, str) != 0) {
+         prev_ptr = prev_ptr->next;
+      }
+
+      return prev_ptr;
+   }
 };
 
+int main()
+{
+   ios_base::sync_with_stdio(0), cin.tie(0); 
+
+   HashMap hash_map;
+   int n, m, cnt = 0;
+   char s[MAX_LEN];
+
+   hash_map.init();
+
+   cin >> n >> m;
+   for(int i=0; i<n; ++i) {
+      cin >> s;
+      hash_map.insert(s);
+   }
+
+   for(int i=0; i<m; ++i) {
+      cin >> s;
+      if(hash_map.get(s) != NULL) {
+         cnt++;
+      }
+   }
+
+   cout << cnt << '\n';
+   return 0;
+}
